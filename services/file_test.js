58a69ac5bef6d493c5845f46043af93a31c67239
Ganesha <reekoheek@gmail.com>
2014-11-25 17:36:16 +0700

    introduced file test

--- services/file_test.js
+++ services/file_test.js
@@ -1,8 +1,26 @@
 module.exports = function() {
-    this.from('file:tmp')
-        .to(function(exchange) {
-            console.log('GOT FILE:', exchange.body);
-        });
+    this.from('direct:in')
+        // .to('file:///home/jafar/test');
+        .to('file:test/ignore-001-inbox');
+
+    this.from('file:test/ignore-001-inbox')
+        // .to(function(exchange) {
+        //     console.log('BODY: ' + exchange.body);
+        // });
+        .to('file:test/ignore-002-outbox');
 
     this.trace = true;
+
+    var template = this.createProducerTemplate();
+
+    setImmediate(function() {
+        try {
+            var data = 'one ' + new Date();
+            template.send('direct:in', data);
+            // template.send('direct:in', 'two');
+            // template.send('direct:in', 'three');
+        } catch(e) {
+            console.log(e.stack);
+        }
+    });
 };
